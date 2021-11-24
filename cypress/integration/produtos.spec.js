9fcbfe4810f33fd67efb7f6de88d5a2585974c3d
Igor Brelaz <igbrelaz@gmail.com>
2021-11-24 15:32:22 -0300

    alteraçoes no teste automatizado

---
+++ cypress/integration/produtos.spec.js
@@ -0,0 +1,33 @@
+/// <reference types="cypress" />
+
+describe('Funcionalidade Página de produtos', () => {
+
+    beforeEach(() => {
+        cy.visit('http://lojaebac.ebaconline.art.br/produtos/')
+    });
+
+    it('Deve selecionar um produto da lista', () => {
+        cy.get('[class="product-block grid"]')
+            //.first()
+            //.last()
+            //.eq(3)
+            .contains('Apollo Running Short')
+            .click()
+    });
+
+    it.only('Deve adicionar um produto ao carrinho', () => {
+        var quantidade = 3
+        
+        cy.get('[class="product-block grid"]')
+            .contains('Apollo Running Short')
+            .click()
+        cy.get('.button-variable-item-34').click()
+        cy.get(':nth-child(2) > .value > .variable-items-wrapper > .variable-item').click()
+        cy.get('.input-text').clear().type(quantidade)
+        cy.get('.single_add_to_cart_button').click()
+
+        cy.get('.dropdown-toggle > .mini-cart-items').should('contain', quantidade)
+        cy.get('.woocommerce-message').should('contain', quantidade + ' × “Apollo Running Short” foram adicionados no seu carrinho.')
+    });
+
+});

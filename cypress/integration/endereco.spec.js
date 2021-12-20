16348940074c6594978d9bfa10340e071692a561
Igor Brelaz <igbrelaz@gmail.com>
2021-12-20 15:18:44 -0300

    alteraçao do teste para usar arquivo de dados

--- cypress/integration/endereco.spec.js
+++ cypress/integration/endereco.spec.js
@@ -1,4 +1,6 @@
 /// <reference types="cypress" />
+import EnderecoPage from '../support/page-objects/endereco.page'
+const dadosEndereco = require('../fixtures/endereco.json')
 
 describe('Funcionalidade Endereços - Faturamento e Entrega', () => {
     
@@ -10,7 +12,25 @@ describe('Funcionalidade Endereços - Faturamento e Entrega', () => {
     });
     
     it('Deve fazer o cadastro de faturamento com sucesso', () => {
+        EnderecoPage.editarEnderecoFaturamento('Gabriel', 'Araujo', 'CI&T', 'Brasil', 'Estrada dos Bandeirantes', '23.304', 'Rio de Janeiro', 'Rio de Janeiro', '22785091', '2456-6972', 'gbaraujo@gmail.com')
+        cy.get('.woocommerce-message').should('contain', 'Endereço alterado com sucesso.')
+    });
 
+    it.only('Deve fazer o cadastro de faturamento com sucesso - Usando arquivo de dados', () => {
+        EnderecoPage.editarEnderecoFaturamento(
+            dadosEndereco[1].nome,
+            dadosEndereco[1].sobrenome,
+            dadosEndereco[1].empresa,
+            dadosEndereco[1].pais,
+            dadosEndereco[1].endereco,
+            dadosEndereco[1].numero,
+            dadosEndereco[1].cidade,
+            dadosEndereco[1].estado,
+            dadosEndereco[1].cep,
+            dadosEndereco[1].telefone,
+            dadosEndereco[1].email
+        )
+        cy.get('.woocommerce-message').should('contain', 'Endereço alterado com sucesso.')
     });
 
 });
